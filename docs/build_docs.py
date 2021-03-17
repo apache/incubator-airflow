d252eba6e5b234f246f26bf7ace2a92c2e743b60
Kamil Breguła <kamilbregula@apache.org>
2021-03-17 08:24:08 +0100

    Improvments for Docker Image docs

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -510,7 +510,7 @@ def main():
             docs_only=docs_only,
             spellcheck_only=spellcheck_only,
             for_production=for_production,
-            jobs=jobs,
+            jobs=1,
             verbose=args.verbose,
         )
         if package_build_errors:
