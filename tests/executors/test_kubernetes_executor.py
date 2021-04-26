5ec589ab755a71442a55961557ad91b8dc703e09
EphraimBuddy <splendidzigy24@gmail.com>
2021-04-26 06:55:07 +0100

    Apply review suggestions and add more tests

--- tests/executors/test_kubernetes_executor.py
+++ tests/executors/test_kubernetes_executor.py
@@ -40,6 +40,7 @@ try:
         ResourceVersion,
         create_pod_id,
         get_base_pod_from_template,
+        get_latest_resource_version,
     )
     from airflow.kubernetes import pod_generator
     from airflow.kubernetes.pod_generator import PodGenerator, datetime_to_label_safe_datestring
@@ -674,7 +675,7 @@ class TestKubernetesJobWatcher(unittest.TestCase):
         self._run()
         self.watcher.watcher_queue.put.assert_not_called()
 
-    @mock.patch('airflow.executors.kubernetes_executor.get_resource_version')
+    @mock.patch('airflow.executors.kubernetes_executor.get_latest_resource_version')
     def test_process_error_event(self, mock_get_resource_version):
         mock_get_resource_version.return_value = '43334'
         self.pod.status.phase = 'Pending'
@@ -694,7 +695,7 @@ class TestResourceVersion(unittest.TestCase):
         resource_instance = ResourceVersion(resource_version='4567')
         assert resource_instance.resource_version == '4567'
 
-    @mock.patch('airflow.executors.kubernetes_executor.get_resource_version')
+    @mock.patch('airflow.executors.kubernetes_executor.get_latest_resource_version')
     def test_different_instance_share_state(self, mock_get_resource_version):
         kube_client = mock.MagicMock()
         mock_get_resource_version.return_value = '4566'
@@ -709,3 +710,13 @@ class TestResourceVersion(unittest.TestCase):
         assert resource_instance2.resource_version == '6787'
         assert resource_instance3.resource_version == '6787'
         assert resource_instance4.resource_version == '6787'
+
+
+class TestGetLatestResourceVersion(unittest.TestCase):
+    def test_get_latest_resource_version(self):
+        kube_client = mock.MagicMock()
+        list_namespaced_pod = kube_client.list_namespaced_pod
+        list_namespaced_pod.return_value.metadata.resource_version = '5688'
+        resource_version = get_latest_resource_version(kube_client, 'mynamespace')
+        assert list_namespaced_pod.called
+        assert resource_version == '5688'
